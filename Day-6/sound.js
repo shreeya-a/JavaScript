9a0e9081b2cfcf6807e9faf557c82472919f2270
shreeya-a <shreeyadhikari11@gmail.com>
2023-10-11 20:57:53 +0545

    game assets: sound and images

--- Day-6/sound.js
+++ Day-6/sound.js
@@ -1,8 +1,37 @@
 const canvas = document.getElementById("canvas");
 const c = canvas.getContext("2d");
 
-function animate(){
+//load the music
+// Audio()
+const backgroundMusic = new Audio();
+const gunSound = new Audio();
+backgroundMusic.src = "./sounds/burstfirewav-14443.wav";
+//play the music
 
+//bg music
+function playSound(){
+    backgroundMusic.play();
+    backgroundMusic.loop = true; // plays bg music in loop
+    // backgroundMusic.pause();
+    // backgroundMusic.stop();
+}
+
+//play music on click
+canvas.addEventListener("click", () =>{
+    gunSound.src = "./sounds/duck-quack.wav";
+    gunSound.play();
+    gunSound.volume = 0.2;
+})
+
+//loading image
+const spaceShip = new Image();
+spaceShip.src = "./images/plane.png" ;
+
+let y = 0;
+function animate(){
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.drawImage(spaceShip, 0, y, 50, 50);
+    y++;
     window.requestAnimationFrame(animate);
 }
 animate();
