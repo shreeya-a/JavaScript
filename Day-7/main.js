fd3023f3473c299c1dace91ecbafb8ac552b79b0
shreeya-a <shreeyadhikari11@gmail.com>
2023-10-12 21:35:06 +0545

    Space shooter game

---
+++ Day-7/main.js
@@ -0,0 +1,45 @@
+const canvas = document.getElementById("canvas");
+const c = canvas.getContext("2d");
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+const player = new Player();
+let allBullets = [];
+
+// game loop
+function loop(){
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+
+    for(let i = 0; i< allBullets.length; i++){
+        allBullets[i].update();
+    }
+
+    window.requestAnimationFrame(loop);    //60 times per sec
+}
+loop();
+
+//all listener added below
+// add event listener to document not just canvas
+// don't add event listener inside loop 
+//because every sec 60 frames are changed
+
+document.addEventListener("keydown", (event) => {
+    if(event.code === "ArrowUp") player.velocity.y = -5;
+    if(event.code === "ArrowDown") player.velocity.y = 5;
+    if(event.code === "ArrowLeft") player.velocity.x = -5;
+    if(event.code === "ArrowRight") player.velocity.x = 5;
+    if(event.code === "Space") 
+    allBullets.push(
+        new Bullet(player.position.x + player.size / 5, player.position.y - player.size / 2)
+        );
+});
+
+document.addEventListener("keyup", (event) => {
+    if(event.code === "ArrowUp") player.velocity.y = 0;
+    if(event.code === "ArrowDown") player.velocity.y = 0;
+    if(event.code === "ArrowLeft") player.velocity.x = 0;
+    if(event.code === "ArrowRight") player.velocity.x = 0;
+    
+});
---
+++ Day-7/player.js
@@ -0,0 +1,35 @@
+class Player{
+    constructor(){
+        //init
+        this.position = {
+            x: canvas.width / 2.8,
+            y: canvas.height - 100,
+        };
+        this.velocity = {
+            x: 0,
+            y: 0,
+        };
+        this.size = 60;
+        this.image = new Image();
+        this.image.src = "./images/plane.png";
+    }
+
+
+    draw(){
+        c.beginPath();
+        c.fillStyle = "red";
+        // c.fillRect(this.position.x, this.position.y, this.size, this.size);
+        c.drawImage(this.image, this.position.x, this.position.y, this.size, this.size);
+
+    }   
+    move(){
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+
+    update(){
+        this.draw();    
+        this.move();
+    }
+
+}
